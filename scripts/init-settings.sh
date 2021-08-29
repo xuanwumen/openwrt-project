c47be906035a97347009c3effc178eb7c22ad195
vipboddy <89140270+vipboddy@users.noreply.github.com>
2021-08-30 02:30:56 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -17,7 +17,7 @@ sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release 
 echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release 
 export orig_version="$(cat "/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
-sed -i "s/${orig_version}/${orig_version} By Vip $(date +"%Y-%m-%d")/g" /etc/openwrt_release
+sed -i "s/${orig_version}/${orig_version} By Vip@snapshots $(date +"%Y-%m-%d")/g" /etc/openwrt_release
 
 # Add execute permission for ipv6-helper
 chmod +x /bin/ipv6-helper
