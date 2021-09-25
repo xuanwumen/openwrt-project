97d3302421edc2e8a1de230aae0ba41ea241fe9a
xuanwumen <yj339173747yj2015@outlook.com>
2021-09-26 04:01:27 +0800

    project-lede.sh:del bypass

--- scripts-test/project-lede.sh
+++ scripts-test/project-lede.sh
@@ -36,8 +36,8 @@ git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
 
 # Add luci-app-bypass
-git clone --depth=1 https://github.com/garypang13/luci-app-bypass
-git clone --depth=1 https://github.com/garypang13/smartdns-le
+# git clone --depth=1 https://github.com/garypang13/luci-app-bypass
+# git clone --depth=1 https://github.com/garypang13/smartdns-le
 
 # Add mentohust & luci-app-mentohust
 git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
