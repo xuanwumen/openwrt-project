ec36adab6db71d69e0ff8238c3e3d5846e0a419d
xuanwumen <339173747@qq.com>
2021-10-01 07:20:04 +0000

    project-lede.sh:del rtl8192du

--- scripts-test/project-lede.sh
+++ scripts-test/project-lede.sh
@@ -93,7 +93,7 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8821cu
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8188eu
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8192du
+# svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8192du
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl88x2bu
 
 # Add apk (Apk Packages Manager)
