53a13a31af42c9d7879fdb25f47623b627dc2d4b
xuanwumen <45932714+xuanwumen@users.noreply.github.com>
2021-12-13 14:11:19 +0800

    Update init-nas-settings.sh

--- scripts-test/init-nas-settings.sh
+++ scripts-test/init-nas-settings.sh
@@ -21,7 +21,7 @@ sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release 
 echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release 
 export orig_version="$(cat "/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
-sed -i "s/${orig_version}/${orig_version} By Vip@nas $(date +"%Y-%m-%d")/g" /etc/openwrt_release
+sed -i "s/${orig_version}/${orig_version} By Vip@mini $(date +"%Y-%m-%d")/g" /etc/openwrt_release
 
 
 
