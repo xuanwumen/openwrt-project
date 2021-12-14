fd5afd87bcfb3531839aa154b5103755dbef9998
xuanwumen <45932714+xuanwumen@users.noreply.github.com>
2021-12-15 01:37:41 +0800

    Check file system during boot

--- scripts-test/init-settings.sh
+++ scripts-test/init-settings.sh
@@ -9,12 +9,12 @@
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
+# Check file system during boot
+uci set fstab.@global[0].check_fs=1
+uci commit
+
 # Disable opkg signature check
 sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
-
-# Disable autostart by default for some packages
-# cd /etc/rc.d
-# rm -f S98udptools || true
 
 # Disable IPV6 ula prefix
 sed -i 's/^[^#].*option ula/#&/' /etc/config/network
