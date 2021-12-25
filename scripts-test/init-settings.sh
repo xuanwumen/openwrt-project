7a57a1b6335d1f50902ab4c23263727d881c82af
xuanwumen <45932714+xuanwumen@users.noreply.github.com>
2021-12-26 04:33:09 +0800

    Update init-settings.sh

--- scripts-test/init-settings.sh
+++ scripts-test/init-settings.sh
@@ -18,6 +18,11 @@ sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 
 # Disable IPV6 ula prefix
 sed -i 's/^[^#].*option ula/#&/' /etc/config/network
-
+# Add build date to index page
+sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release 
+sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release 
+echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release 
+export orig_version="$(cat "/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} By Vip@test $(date +"%Y-%m-%d")/g" /etc/openwrt_release
 
 exit 0
