b45fd0645a484e52d194fbb11f6cf0a8f755e4ae
xuanwumen <45932714+xuanwumen@users.noreply.github.com>
2022-02-19 05:18:15 +0800

    Update hook-feeds.sh

--- scripts-test/hook-feeds.sh
+++ scripts-test/hook-feeds.sh
@@ -7,6 +7,16 @@
 
 # Svn checkout packages from immortalwrt's repository
 pushd customfeeds
+# add baidupcs-web verysync
+rm -rf packages/net/baidupcs-web
+rm -rf packages/net/verysync
+rm -rf luci/applications/luci-app-baidupcs-web
+rm -rf luci/applications/luci-app-verysync
+svn co https://github.com/xuanwumen/packages/trunk/baidupcs-web packages/net/baidupcs-web
+svn co https://github.com/xuanwumen/packages/trunk/luci-app-baidupcs-web luci/applications/luci-app-baidupcs-web
+svn co https://github.com/xuanwumen/packages/trunk/luci-app-verysync luci/applications/luci-app-verysync
+svn co https://github.com/xuanwumen/packages/trunk/verysync packages/net/verysync
+
 mkdir temp
 git clone --depth=1 https://github.com/immortalwrt/packages -b openwrt-18.06 temp/packages
 git clone --depth=1 https://github.com/immortalwrt/luci -b openwrt-18.06-k5.4 temp/luci
@@ -21,16 +31,16 @@ cp -r temp/packages/net/adguardhome packages/net/adguardhome
 # Add luci-app-eqos
 cp -r temp/luci/applications/luci-app-eqos luci/applications/luci-app-eqos
 
+# Add luci-app-syncthing
+cp -r temp/luci/applications/luci-app-syncthing luci/applications/luci-app-syncthing
+
 # Add luci-app-ipsec-vpnserver-manyusers
 cp -r temp/luci/applications/luci-app-ipsec-vpnserver-manyusers luci/applications/luci-app-ipsec-vpnserver-manyusers
 
 # Add  themes
-rm -rf ../package/lean/luci-theme-argon
+rm -rf luci/themes/luci-theme-argon
+git clone  --depth=1 https://github.com/kiddin9/luci-theme-edge -b 18.06 luci/themes/luci-theme-edge
 cp -r temp/luci/themes/luci-theme-argonv3 luci/themes/luci-theme-argonv3
-cp -r temp/luci/themes/luci-theme-argon luci/themes/luci-theme-argon
-cp -r temp/luci/themes/luci-theme-argonv2 luci/themes/luci-theme-argonv2
-cp -r temp/luci/themes/luci-theme-argon-mod luci/themes/luci-theme-argon-mod
-cp -r temp/luci/themes/luci-theme-argon-mc luci/themes/luci-theme-argon-mc
 cp -r temp/luci/applications/luci-app-argon-config luci/applications/luci-app-argon-config
 cp -r temp/luci/themes/luci-theme-atmaterial luci/themes/luci-theme-atmaterial
 cp -r temp/luci/themes/luci-theme-Butterfly luci/themes/luci-theme-Butterfly
@@ -39,7 +49,6 @@ cp -r temp/luci/themes/luci-theme-infinityfreedom luci/themes/luci-theme-infinit
 cp -r temp/luci/themes/luci-theme-opentomato luci/themes/luci-theme-opentomato
 cp -r temp/luci/themes/luci-theme-opentomcat luci/themes/luci-theme-opentomcat
 cp -r temp/luci/themes/luci-theme-rosy luci/themes/luci-theme-rosy
-cp -r temp/luci/themes/luci-theme-bootstrap-mod luci/themes/luci-theme-bootstrap-mod
 
 # Add luci-app-gowebdav
 cp -r temp/luci/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
@@ -47,7 +56,7 @@ cp -r temp/packages/net/gowebdav packages/net/gowebdav
 
 # Add netdata
 rm -rf packages/admin/netdata
-rm -rf ../package/lean/luci-app-netdata
+rm -rf luci/applications/luci-app-netdata
 cp -r temp/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
 cp -r temp/packages/admin/netdata packages/admin/netdata
 
@@ -61,6 +70,9 @@ cp -r temp/packages/libs/msgpack-c packages/libs/msgpack-c
 
 # Add gotop
 cp -r temp/packages/admin/gotop packages/admin/gotop
+
+# Add luci-app-xunlei
+cp -r temp/luci/applications/luci-app-xunlei luci/applications/luci-app-xunlei
 
 # Add minieap
 cp -r temp/packages/net/minieap packages/net/minieap
@@ -80,8 +92,5 @@ sed -i '/src-git luci/d' feeds.conf.default
 echo "src-link luci $luci_feed" >> feeds.conf.default
 
 # Update feeds
-
-# sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/x86/Makefile
-
 # sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/rockchip/Makefile
 ./scripts/feeds update -a
