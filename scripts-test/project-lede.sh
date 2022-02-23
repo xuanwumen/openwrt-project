d738914aa71b90413f2bf686b23fc8a8b52eb5cf
xuanwumen <45932714+xuanwumen@users.noreply.github.com>
2022-02-23 22:04:34 +0800

    Update project-lede.sh

--- scripts-test/project-lede.sh
+++ scripts-test/project-lede.sh
@@ -49,7 +49,7 @@ git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
 git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
 
 # Add luci-proto-minieap
-git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
+# git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
 
 # Add ServerChan
 # git clone --depth=1 https://github.com/tty228/luci-app-serverchan
